542fb4d7959411cc6a0471bf42735eff9be66852
SithikaH <147823969+SithikaH@users.noreply.github.com>
2025-04-30 23:33:18 +0530

    Add files via upload

---
+++ ForgotPasswordDialog.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class ForgotPasswordDialog : Window
+    {
+        public ForgotPasswordDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            DialogBox dialog = new DialogBox("A password reset link has been sent to the email associated with the username.");
+            dialog.Owner = this;
+            dialog.ShowDialog();
+            this.Close();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (UsernameTextBox.Text == "")
+            {
+                UsernamePlaceholder.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(UsernameTextBox.Text))
+            {
+                UsernamePlaceholder.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
